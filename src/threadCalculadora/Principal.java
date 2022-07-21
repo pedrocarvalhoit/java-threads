8c56d5a1151e68e88b493993112d838c06199c38
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-07-21 18:50:31 +0100

    Primeiro commit dos estudos

---
+++ src/threadCalculadora/Principal.java
@@ -0,0 +1,13 @@
+package threadCalculadora;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+			
+		System.out.println("Isso é um thread main");
+		
+		Thread.sleep(30000);
+
+	}
+
+}
